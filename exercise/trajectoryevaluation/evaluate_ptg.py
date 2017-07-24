0d4b0090a2db6fa992acdbd597c250dfccbd0007
Levin Jian <jianzhirong@gmail.com>
2017-07-25 07:53:11 +0800

    trajectory generation, follow vehicle

--- exercise/trajectoryevaluation/evaluate_ptg.py
+++ exercise/trajectoryevaluation/evaluate_ptg.py
@@ -4,17 +4,28 @@ from ptg import follow_vehicle
 from helpers import Vehicle, show_trajectory
 
 def main():
-	vehicle = Vehicle([0,10,0, 0,0,0])
+# 	vehicle = Vehicle([0,10,0, 0,0,0])
+# 	predictions = {0: vehicle}
+# 	target = 0
+# 	delta = [-20, 0, 0, 0, 0 ,0]
+# 	start_s = [10, 10, 0]
+# 	start_d = [4, 0, 0]
+	
+	
+	#keep lane, constraint
+	vehicle = Vehicle([50,10,0, 0,0,0])
 	predictions = {0: vehicle}
 	target = 0
-	delta = [-20, 0, 0, 0, 0 ,0]
-	start_s = [10, 10, 0]
-	start_d = [4, 0, 0]
+	delta = [-3, 0, 0, 0, 0 ,0]
+	start_s = [0, 10, 0]
+	start_d = [0, 0, 0]
 	
 	
 	T = 5.0
 	best = follow_vehicle(start_s, start_d, T, target, delta, predictions)
+	print("best trajectory {}".format(best))
 	show_trajectory(best[0], best[1], best[2], vehicle)
+	
 
 if __name__ == "__main__":
 	main()
