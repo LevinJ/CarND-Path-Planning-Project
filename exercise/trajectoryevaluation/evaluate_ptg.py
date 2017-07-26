3e962525090651930f6e959de73cce207d96846e
Levin Jian <jianzhirong@gmail.com>
2017-07-26 14:08:04 +0800

    clean up the test cases for trajectory generation

--- exercise/trajectoryevaluation/evaluate_ptg.py
+++ exercise/trajectoryevaluation/evaluate_ptg.py
@@ -8,67 +8,34 @@ def main():
 	
 	
 	
-	#keep lane, follow vehicle
-# 	vehicle = Vehicle([50,10,0, 0,0,0])
-# 	predictions = {0: vehicle}
-# 	target = 0
-# 	delta = [-3, 0, 0, 0, 0 ,0]
-# 	start_s = [0, 10, 0]
-# 	start_d = [0, 0, 0]
-# 	best = follow_vehicle(start_s, start_d, T, target, delta, predictions)
-	
-	
-	#keep lane,follow goal
-	
-# 	vehicle = Vehicle([1500,10,0, 0,0,0])
-# 	predictions = {0: vehicle}
-# 	start_s = [0, 10, 0]
-# 	start_d = [0, 0, 0]
-# 	goal_s = [130,30,0]
-# 	goal_d = [0,0,0]
+
 
 	
 	
-# 	vehicle = Vehicle([1500,10,0, 0,0,0])
-# 	predictions = {0: vehicle}
-# 	start_s = [0, 10, 0]
-# 	start_d = [0, 0, 0]
-# 	best = keep_lane(start_s, start_d, T, predictions)
+	vehicle = Vehicle([1500,10,0, 0,0,0])
+	predictions = {0: vehicle}
+	start_s = [0, 10, 0]
+	start_d = [0, 0, 0]
+	best = keep_lane(start_s, start_d, T, predictions)
 
 
 
-# 	vehicle = Vehicle([50,10,0, 0,0,0])
+# 	vehicle = Vehicle([50,10,0, 2,0,0])
 # 	predictions = {0: vehicle}
 # 	start_s = [0, 10, 0]
-# 	start_d = [0, 0, 0]
-# 	best = keep_lane(start_s, start_d, T, predictions)
-	
-	
-	
-	#PLCL, with target
-# 	vehicle = Vehicle([50,20,0, 6,0,0])
-# 	vehicle_1 = Vehicle([50,10,0, 2,0,0])
-# 	predictions = {0: vehicle, 1:vehicle_1}
-# 	start_s = [30, 10, 0]
 # 	start_d = [2, 0, 0]
-# 	best = PLCL(start_s, start_d, T, predictions)
+# 	best = keep_lane(start_s, start_d, T, predictions)
 	
 	
-	#PLCR, with target
+	
+	
+	
 # 	vehicle = Vehicle([50,20,0, 2,0,0])
 # 	vehicle_1 = Vehicle([50,10,0, 6,0,0])
 # 	predictions = {0: vehicle, 1:vehicle_1}
 # 	start_s = [30, 10, 0]
 # 	start_d = [6, 0, 0]
-# 	best = LC(start_s, start_d, T, predictions, prepare=True, left= False)
-	
-	
-	vehicle = Vehicle([50,20,0, 2,0,0])
-	vehicle_1 = Vehicle([50,10,0, 6,0,0])
-	predictions = {0: vehicle, 1:vehicle_1}
-	start_s = [30, 10, 0]
-	start_d = [6, 0, 0]
-	best = LC(start_s, start_d, T, predictions, prepare=False, left= True)
+# 	best = LC(start_s, start_d, T, predictions, prepare=False, left= False)
  	
 	
 	
